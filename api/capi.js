4b3f43416e979c94c92487e169271f5ff8ca6b18
sambuev <6326685+sambuev@users.noreply.github.com>
2025-09-21 05:46:24 -0300

    Update capi.js v16

--- api/capi.js
+++ api/capi.js
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
   // CORS Headers
   res.setHeader('Access-Control-Allow-Origin', 'https://spindigitals.com');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, X-Requested-With');
 
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
